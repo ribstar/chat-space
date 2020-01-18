a4233010670b7f45b825ecb41145fc4a9f58280a
ribstar <wood.sk.love315@iclud.com>
2020-01-18 16:37:59 +0900

    自動更新機能　実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,25 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="area-message__2-box">
-           <div class="area-message__2-box__user-name">
-             ${message.user_name}
-           </div>
-           <div class="area-message__2-box__time">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="area-message__text">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="area-message__2-box">
-           <div class="area-message__2-box__user-name">
-             ${message.user_name}
-           </div>
-           <div class="area-message__2-box__time">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="area-message__text">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+    var content = message.content ? `${ message.content }` : "";
+      var img  = message.image ? `<img class="lower-message__image" src="${ message.image }">` : "";
+      var html = `<div class="message" data-message-id="${message.id}">
+                    <div class="area-message__2-box">
+                      <p class="area-message__2-box__user-name">
+                        ${message.user_name}
+                      </p>
+                      <p class="area-message__2-box__time">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                      <div class="area-message__text">
+                        <p class="lower-message__content">
+                            ${content}
+                        </p>
+                            ${img}
+                      </div>
+                  </div>`
+    return html;
+    }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -64,5 +45,31 @@ $(function(){
       $('form').find(':submit').removeAttr("disabled");
     })
   });
-  
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.area-message').append(insertHTML);
+        $('.area-message').animate({ scrollTop: $('.area-message')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
